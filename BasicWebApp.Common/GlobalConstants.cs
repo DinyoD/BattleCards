ff2463d483ddc60bde070174125b58d236acadca
DinyoD <40523718+DinyoD@users.noreply.github.com>
2021-08-27 16:29:16 +0300

    Initial Create.

---
+++ BasicWebApp.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicWebApp.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "Battle Cards";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
